6b449d15946582826a82d730f661ff21a916cf8c
Grace Carandang <grace.carandang@outlook.com>
2025-02-17 23:00:05 +0000

    playing around with config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   css:{
     modules: {
       localsConvention: "camelCase"
     }
   },
-  base: "/sorbetes.github.io",
 })
