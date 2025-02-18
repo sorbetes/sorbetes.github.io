73c82667581fa12c4deef9a3010491277eea77e9
Grace Carandang <grace.carandang@outlook.com>
2025-02-18 23:45:17 +0000

    font size

--- src/components/experience/ExperienceHeading.tsx
+++ src/components/experience/ExperienceHeading.tsx
@@ -13,13 +13,13 @@ export const ExperienceHeading = ({
 }: ExperienceHeadingProps) => {
   return (
     <div className="text-center w-full md:w-1/3 hidden md:flex flex-col items-center justify-center sticky h-screen top-0 self-start">
-      <p className=" text-[2rem] md:text-[2.5rem] lg:text-[3rem] font-semibold text-black">
+      <p className=" text-[2rem] md:text-[2.5rem] font-semibold text-black">
         {company}
       </p>
-      <p className="text-[1.5rem] md:text-[2rem] lg:text-[2.25rem] font-medium text-black">
+      <p className="text-[1.5rem] md:text-[2rem] font-medium text-black">
         {jobTitle}
       </p>
-      <p className="text-[1.5rem] md:text-[2rem] lg:text-[2.25rem] font-light text-black">
+      <p className="text-[1.5rem] md:text-[2rem] font-light text-black">
         {startDate} - {endDate ?? "Present"}
       </p>
     </div>
