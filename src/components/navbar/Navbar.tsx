311aefc681f594a86e8025ddeec2c0b15d11f7af
Grace Carandang <grace.carandang@outlook.com>
2025-04-15 11:44:49 +0100

    hide navbar for small screens

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
   };
 
   return (
-    <div className="fixed top-0 w-full h-[7vh] z-[10000] bg-[rgba(209, 217, 230, 0.5)] shadow-md backdrop-blur-xl">
+    <div className="hidden md:block fixed top-0 w-full h-[7vh] z-[10000] bg-[rgba(209, 217, 230, 0.5)] shadow-md backdrop-blur-xl">
       <div className="flex w-[80%] mx-auto h-full">
         <nav className="flex items-center justify-between w-full">
           <h1 className="flex-[0.6] cursor-pointer text-[25px] text-black">
