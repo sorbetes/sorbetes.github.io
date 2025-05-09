d172de374c0859c2b2802a37cb90238955ac4730
Grace Carandang <grace.carandang@outlook.com>
2025-05-09 16:14:10 +0100

    slight tweaks

--- src/data/aboutMeList.ts
+++ src/data/aboutMeList.ts
@@ -2,11 +2,11 @@ export const aboutMeBlurbs = [
   [
     `Hi, I'm Grace, full time SaaS Engineer, part time serial-hobbyist.`,
     `When I'm not writing code, you'll probably find me deep in a new craft project; baking, playing a new board game, or crocheting yet another blanket I definitely don't need. I'm naturally curious and love learning for the fun of it, always throwing myself into something new.`,
-    `A jack-of-all-trades, with an eye for this-and-that, which is probably why full-stack engineering feels like home. I wouldn’t trade that chaotic joy for anything.`,
+    `A jack-of-all-trades, with an eye for this-and-that, full-stack engineering feels like home. I wouldn’t trade that chaotic joy for anything.`,
   ],
   [
-    `Take a gander at my exporiences below. But beyond the tools and tech and titles, what really drives me is the joy of problem-solving, and the fear of stagnation.`,
-    `I pride myself in being able to pick something up, and run with it. That's why I thrive in fast-paced, ever-evolving environments where I can explore, experiment, and build things that make life easier.`,
+    `I pride myself on being able to pick things up and run with them. It's why I thrive in fast-paced, ever-evolving environments where I can explore, experiment, and build things that make life easier.`,
+    `Take a gander at my experiences below. But beyond the tools and tech, and titles, what really drives me is the joy of problem-solving and the fear of stagnation.`,
   ],
   [
     `And the most important thing about me is: I love food. Like, *really* love it. Tech is great, but a bowl of beef pho? That’s the kind of magic I live for.`,
